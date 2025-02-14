146b6f7e71eb325de5008f3d925a8c6a94344f90
Andrii Semenenko <oxonomy14@gmail.com>
2025-02-14 14:21:02 +0700

    fixed3

--- src/js/loader.js
+++ src/js/loader.js
@@ -1,6 +1,6 @@
 // Функкції показу/ховання лоадера
 export function showLoader() {
-  document.querySelector('#image-search').classList.add('hidden'); // Показуєм контент
+  document.querySelector('#image-search').classList.add('hidden'); // Ховаємо контент
   document.querySelector('.loader-container').style.display = 'flex'; // Показуєм лоадер
 }
 
